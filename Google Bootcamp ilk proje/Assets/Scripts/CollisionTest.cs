0bf2911a5135cd54b2f17b1000f9f7d5185257f0
kaandonmez <kaandonmeztf@gmail.com>
2022-07-02 14:17:38 +0300

    tested

---
+++ Google Bootcamp ilk proje/Assets/Scripts/CollisionTest.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionTest : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("Player entered the trigger");
+            //destroy the game object
+            Destroy(gameObject);
+        }
+    }
+}
